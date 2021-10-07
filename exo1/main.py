6a2ef0aba837930d3bcd5063f75ed05a4dc9848f
Alexandre Chetafi <alexandre.chetafi@epitech.eu>
2021-10-07 04:58:14 +0200

    corrections

--- exo1/main.py
+++ exo1/main.py
@@ -6,12 +6,12 @@ rng = numpy.random.default_rng()
 
 def probability(state, new_state, time):
     if time <= 0:
-        return time, 0
+        return 0, 0
     cost_actual_state = cost_function(state)
     cost_new_state = cost_function(new_state)
     if cost_new_state < cost_actual_state:
-        return time - 0.1, 1
-    return time - 0.1, math.exp(-(cost_new_state - cost_actual_state) / time)
+        return time - 0.01, 1
+    return time - 0.01, math.exp(-(cost_new_state - cost_actual_state) / time)
 
 
 def cost_function(state):
@@ -26,12 +26,12 @@ def cost_function(state):
 def __test__cost_function():
     state = [-8, 1, 2, -1, 50]
     if cost_function(state) != 72:  # somme cumulative
-        raise
+        raise Exception('Bad sum')
 
 
 def two_opt(state, minimum, maximum):
     """Take [min, max] to inverse"""
-    if not minimum < maximum or minimum == 0 or maximum == len(state):
+    if not minimum < maximum or minimum < 0 or maximum == len(state):
         raise Exception('min < max')
     new_state = state[:minimum]
     for i in range(maximum, minimum - 1, -1):
@@ -43,22 +43,19 @@ def two_opt(state, minimum, maximum):
 def __test__two_opt():
     s = [chr(i) for i in range(65, 91)]
     print(s)
-    section = rng.integers(low=1, high=len(s) - 2, size=2).tolist()
+    section = rng.integers(low=0, high=len(s), size=2).tolist()
     print(section)
     ss = two_opt(s, min(section), max(section))
     print(ss)
     if len(s) != len(ss) or len(ss) != len(set(ss)) or len(s) != len(set(s)):
-        raise
+        raise Exception('Replication or omission')
 
 
-def run(beginning_state):
-    print('Initial cost : ', cost_function(beginning_state))
-    print(beginning_state)
-    time = 100
+def run(beginning_state, time=1000):
     state = beginning_state
     new_state = None
     while time >= 0:
-        section = rng.integers(low=1, high=len(state) - 2, size=2).tolist()
+        section = rng.integers(low=0, high=len(state), size=2).tolist()
         if section[0] == section[1]:
             continue
         new_state = two_opt(state, min(section), max(section))
@@ -68,13 +65,16 @@ def run(beginning_state):
         else:
             if prob > rng.random():
                 state = new_state
-    print('Final Cost : ', cost_function(new_state))
+    return new_state, cost_function(new_state)
+
+
+def parcours(initial_state):
+    print('Initial cost : ', cost_function(initial_state))
+    print(initial_state)
+    new_state, cost = run(initial_state)
+    print('Final Cost : ', cost)
     print(new_state)
 
 
-def parcours(list):
-    run(list)
-
-
 if __name__ == '__main__':
-    run(numpy.random.normal(0, 1000, 1000).tolist())
+    parcours(numpy.random.normal(0, 1000, 1000).tolist())
